c24a29e726f5d573606f409b651abd8eb09c6795
Itzik Ephraim <oranja@gmail.com>
2018-06-27 17:31:47 +0300

    Enable notifications

--- webview.js
+++ webview.js
@@ -10,4 +10,21 @@ module.exports = Franz => {
 
   Franz.injectCSS(path.join(__dirname, 'workplace.css'));
   Franz.loop(getMessages);
+  
+  /* Enable desktop notifications in messenger settings */
+  localStorage["_cs_desktopNotifsEnabled"] = JSON.stringify({"__t":new Date().getTime(), "__v":true})
+
+  if (typeof Franz.onNotify === 'function') {
+    Franz.onNotify((notification) => {
+      if (typeof notification.title !== 'string') {
+        notification.title = ((notification.title.props || {}).content || [])[0] || 'Work Chat';
+      }
+
+      if (typeof notification.options.body !== 'string') {
+        notification.options.body = (((notification.options.body || {}).props || {}).content || [])[0] || '';
+      }
+
+      return notification;
+    });
+  }
 };
